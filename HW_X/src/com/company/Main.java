6ed526729f58241617090a8bf144a53ddaefe645
ymperton <ymperton@gmail.com>
2017-10-30 16:03:08 -0400

    first commit

--- HW_X/src/com/company/Main.java
+++ HW_X/src/com/company/Main.java
@@ -3,6 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        fizzbuzz();
+    }
+
+    private static void fizzbuzz() {
+        boolean flag;
+        for (int i = 0; i < 100; i++) {
+            flag = false;
+            if (i % 3 == 0) {
+                System.out.print("Fizz");
+                flag = true;
+            }
+            if (i % 5 == 0) {
+                System.out.print("Buzz");
+                flag = true;
+            }
+            if (!flag) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
     }
 }
